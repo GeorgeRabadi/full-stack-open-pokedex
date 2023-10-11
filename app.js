df61f9cf66169b6d4c996d1c2b987b8f25e7dbf0
George Rabadi <96255554+GeorgeRabadi@users.noreply.github.com>
2023-10-11 14:20:49 -0400

    health check

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
 app.use(express.static('dist'))
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
